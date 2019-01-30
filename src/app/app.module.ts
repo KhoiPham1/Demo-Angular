0bb38ce6af1d71e9f7470b4284fe433e4658dad4
Khoi Pham <khoi171195@gmail.com>
2019-01-30 08:32:46 +0700

    color-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { FontSizeComponent } from './font-size/font-size.component';
 import { PetComponent } from './pet/pet.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeComponent,
     PetComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
