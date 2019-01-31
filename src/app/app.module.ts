f5bc0d8793cbbb4664d7a8a3c469f5d5e4efc52d
Khoi Pham <khoi171195@gmail.com>
2019-01-31 11:03:55 +0700

    Heroes-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FontSizeComponent } from './font-size/font-size.component';
 import { PetComponent } from './pet/pet.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ColorPickerComponent } from './color-picker/color-picker.component';
     FontSizeComponent,
     PetComponent,
     CalculatorComponent,
-    ColorPickerComponent
+    ColorPickerComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
